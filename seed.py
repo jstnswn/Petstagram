b96bbbc73f437cda4dad2fbbb25d79660f8bbc80
Chris Young <ChrisC.Young1@gmail.com>
2022-03-04 14:00:32 -0800

    added seed data

---
+++ seed.py
@@ -0,0 +1,221 @@
+# ----------------Users
+
+User(
+    username = 'MisterJackson',
+    full_name = 'Jackson Young-Summers',
+    email = 'Jackson@Young-Summers.com',
+    password = 'password',
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/d3fcf1ade62e4db5936cd484616af0f3.png'
+),
+User(
+    username = 'Mango',
+    full_name = 'Mango Voisin',
+    email = 'Mango@Voisin.com',
+    password = 'password',
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/f2f34ca48fc94467b93d815e299475e6.jpg'
+),
+
+# ----------------Posts
+Post(
+    user_id = 2,
+    caption = "Enjoying some time in the sun. Isn't this a nice tree?",
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/f0db658dd3b14ce19f2a1d5c00b51ef5.jpg',
+),
+
+Post(
+    user_id = 2,
+    caption = "I think I might have found something. I'm on the hunt!",
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/64fd6738ceb7425d839fdba32180cf2c.jpg',
+),
+
+Post(
+    user_id = 2,
+    caption = "It's Christmas time, but this bird has my intrest",
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/5e288da868f343b995c168aa9b7eb972.jpg',
+),
+
+Post(
+    user_id = 2,
+    caption = 'This is a nice leg',
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/8553da5dd6e64126bd796c95b38d96da.jpeg',
+),
+
+Post(
+    user_id = 2,
+    caption = "I'm a bag kitty!",
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/8120c8697c5d41f4a5ec4abf37c30c53.jpeg',
+),
+
+Post(
+    user_id = 2,
+    caption = 'Basking in the sun!',
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/557e3f15417d491c83613c5208b9f91f.jpeg',
+),
+
+Post(
+    user_id = 2,
+    caption = 'Enjoying the view out the front window.',
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/7c619aaae6b743f4bee3965e7536a8b9.jpeg',
+),
+
+Post(
+    user_id = 2,
+    caption = "I found the secret stash!! Hope my human doesn't find out",
+    image_url = 'http://ig-clone-bucket.s3.amazonaws.com/seeds/13a7b0948479481ca0300e954b2e380a.jpeg',
+),
+
+Post(
+    user_id = 2,
+    caption = "Give me attention, human!!",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/eb59a41b99784855803212597de00edf.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "What do you think? Is this my good side?",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/47e52355dc454a21bd357056016112d7.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "Relaxing on my gamer chair",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/1f801885188d4ef6a4be2c4b405523e4.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "I'm a Box kitty this time!",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/d28effd3155b4317b89ad27770ab52db.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "If I fit, I sit.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/80b67e9e91654541be07abd05c437f23.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "I'm beautiful!!!",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/b39686ed67c349aab9d69ef2afaa586d.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "I don't know what this creature is, but it's terrifying!!!",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/157da78b5025437689f6949b632fe3d3.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "They found me in my secret hiding place.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/4e4d99406a69419d96dba15f6b6afd23.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "Such a cosy blanket.....Only $25.99 at target.com",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/0ee29acf09654a2e88ba48d0b9721159.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "My humans made me pose here.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/3cc238c3136b4b1d9e20245966a6e80a.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "How do you like my bow?",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/95b080a34af74ffab4954f444fa38817.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "These birds a huge!! Think I can still catch them? I do.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/f4cbf159ed2e4486a6b2cbde3dce16f8.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "Attention please........",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/d00e8d00ea2448b0bae31976ae4e69e7.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "I think my roommate caught me sleeping in this one.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/8e7f836c280a40b3b85cd31712e99ec1.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "Are you a back sleeper or a side sleeper? I'm both.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/a3f92c4d6ac44679a1a84644ed9c5e4c.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "I know my belly looks inviting, but beware, touch the belly and get the teefies",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/714c3e0d885844088b6b7e6a3cd0b479.jpeg"
+),
+
+Post(
+    user_id = 2,
+    caption = "This is my home inside my home. Holloween edition",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/2e36e53f62814686b67c908b912c5324.jpeg"
+),
+
+Post(
+    user_id = 1,
+    caption = "Just me and my friend chillin' on the deck",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/0a2c506a141743708c5fc25b19669c62.png"
+),
+
+Post(
+    user_id = 1,
+    caption = "Ahh, to be young again....",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/dddcacfaa5584b33b4efe3f6c831e95e.png"
+),
+
+Post(
+    user_id = 1,
+    caption = "I only do this when my roommates laundry is clean. Otherwise too stinky!!",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/d181e18f2c41480a93b5227de6146eb8.png"
+),
+
+Post(
+    user_id = 1,
+    caption = "This is my zen garden, my meditation space.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/579bb752329d4137a4f051e869e66da4.png"
+),
+
+Post(
+    user_id = 1,
+    caption = "My roommates always call me a trash kitty and I don't know why.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/5b115c4768a74135a19c149f79e47fac.png"
+),
+
+Post(
+    user_id = 1,
+    caption = "It's warm in here and there's always a light buzz.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/d1e65c394cc44f64ac36b1d46d93b132.png"
+),
+
+Post(
+    user_id = 1,
+    caption = "Cosy feet, and not too stinky.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/f0480333282b49768975efdfd585aafc.png"
+),
+
+Post(
+    user_id = 1,
+    caption = "I just woke up and my roommates already taking pictures of me.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/ff4b89331f1f48cd9b01de51dc7f3594.png"
+),
+
+Post(
+    user_id = 1,
+    caption = "Kind of weird how much my roommate likes to take picures of me while I sleep.",
+    image_url = "http://ig-clone-bucket.s3.amazonaws.com/seeds/9230783037f745e899ca6f90d5ad7d97.png"
+),
