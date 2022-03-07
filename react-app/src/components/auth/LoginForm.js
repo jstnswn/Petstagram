542ad47d73936a179d535987c9ab5934dfb1f12f
justin chau <jchau623@gmail.com>
2022-03-07 17:59:35 -0500

    login page almost complete

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
+import './LoginForm.css'
+import picture from '../../assets/login-pic.png'
 
 const LoginForm = () => {
   const [errors, setErrors] = useState([]);
@@ -30,35 +32,55 @@ const LoginForm = () => {
     return <Redirect to='/' />;
   }
 
+  if (email !== '' && password !== '') {
+    const button = document.querySelector('#login-btn')
+    button.style.backgroundColor = '#0095f6'
+  }
+
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div id='login-form-page'>
+      <div className='right-col'>
+        <div className='right'>
+          <div className='login-form-container'>
+            <div id='login-form-title'>Petstagram</div>
+            <form onSubmit={onLogin}>
+              <div>
+                {errors.map((error, ind) => (
+                  <div key={ind}>{error}</div>
+                ))}
+              </div>
+              <div>
+                <input
+                  className='login-form-field'
+                  name='email'
+                  type='text'
+                  placeholder='Email or username'
+                  value={email}
+                  onChange={updateEmail}
+                />
+              </div>
+              <div>
+                <input
+                  className='login-form-field'
+                  name='password'
+                  type='password'
+                  placeholder='Password'
+                  value={password}
+                  onChange={updatePassword}
+                />
+                <button id='login-btn' type='submit'>Log In</button>
+              </div>
+            </form>
+          </div>
+          <div className='login-form-container' id='have-account'>Don't have an account?<a href='' id='login-link'>Sign up</a></div>
+        </div>
       </div>
-      <div>
-        <label htmlFor='email'>Email</label>
-        <input
-          name='email'
-          type='text'
-          placeholder='Email'
-          value={email}
-          onChange={updateEmail}
-        />
+      <div className='left-col'>
+        <div className='left'>
+          <img id='img' src={picture}></img>
+        </div>
       </div>
-      <div>
-        <label htmlFor='password'>Password</label>
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type='submit'>Login</button>
-      </div>
-    </form>
+    </div>
   );
 };
 
