6adaa35901528ded008b36e212dfe80a83efa6b6
Chris Young <ChrisC.Young1@gmail.com>
2022-03-07 12:14:34 -0800

    signup form styled

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -2,29 +2,37 @@ import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
+import './SignUpForm.css'
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
   const [username, setUsername] = useState('');
+  const [fullName, setFullName] = useState('')
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
+  // const [repeatPassword, setRepeatPassword] = useState('');
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
+    // if (password === repeatPassword) {
+      console.log(fullName)
+      const data = await dispatch(signUp(username, email, password, fullName));
       if (data) {
         setErrors(data)
       }
-    }
+    // }
   };
 
   const updateUsername = (e) => {
     setUsername(e.target.value);
   };
+
+  const updateFullName = (e) => {
+    // console.log(e.target.value)
+    setFullName(e.target.value);
+  }
 
   const updateEmail = (e) => {
     setEmail(e.target.value);
@@ -34,60 +42,85 @@ const SignUpForm = () => {
     setPassword(e.target.value);
   };
 
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
+  // const updateRepeatPassword = (e) => {
+  //   setRepeatPassword(e.target.value);
+  // };
 
   if (user) {
     return <Redirect to='/' />;
   }
 
   return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div id='signup-form-page'>
+      <div className='form-container'>
+        <div id='signup-form-title'>Petstagram</div>
+        <div className='signup-form-heading'>Sign up to see photos and videos from your friends.</div>
+        <form onSubmit={onSignUp}>
+          <div>
+            {errors.map((error, ind) => (
+              <div key={ind}>{error}</div>
+            ))}
+          </div>
+          <div>
+            {/* <label className='label'>Email</label> */}
+            <input
+              className='form-field'
+              placeholder='Email'
+              type='text'
+              name='email'
+              onChange={updateEmail}
+              value={email}
+            ></input>
+          </div>
+          <div>
+            {/* <label>Full Name</label> */}
+            <input
+              className='form-field'
+              placeholder='Full Name'
+              type='text'
+              name='fullName'
+              onChange={updateFullName}
+              value={fullName}
+            ></input>
+          </div>
+          <div>
+            {/* <label>User Name</label> */}
+            <input
+              className='form-field'
+              placeholder='Username'
+              type='text'
+              name='username'
+              onChange={updateUsername}
+              value={username}
+            ></input>
+          </div>
+          <div>
+            {/* <label>Password</label> */}
+            <input
+              className='form-field'
+              placeholder='Password'
+              type='password'
+              name='password'
+              onChange={updatePassword}
+              value={password}
+            ></input>
+          </div>
+          {/* <div>
+            <label>Repeat Password</label>
+            <input
+              type='password'
+              name='repeat_password'
+              onChange={updateRepeatPassword}
+              value={repeatPassword}
+              required={true}
+            ></input>
+          </div> */}
+          <button id='signup-btn' type='submit'>Sign Up</button>
+        </form>
+        <div className='signup-form-heading' id='signup-form-footing'>By signing up, you agree to view many cute photos of pets.</div>
       </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+      <div className='form-container' id='have-account'>Have an account?<a href='' id='login-link'>Log in</a></div>
+    </div>
   );
 };
 
