c74e555dbd9fcb3bec2e2610994ef414feb1631c
jstnswn <jstnswn@gmail.com>
2022-03-06 19:28:49 -0800

    Add Modal context for forms

---
+++ react-app/src/components/UploadModal/UploadPostForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function UploadPostForm() {
+  return (
+    <form>
+      <label>Image</label>
+      <input
+        type='file'
+      />
+    </form>
+  )
+}
---
+++ react-app/src/components/UploadModal/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { Modal } from '../../context/Modal';
+import UploadPostForm from './UploadPostForm';
+
+export default function UploadModal({ option }) {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => setShowModal(true);
+  const closeModal = () => setShowModal(false);
+
+  return (
+    <>
+      <i className='fa-regular fa-plus upload-button' onClick={openModal}></i>
+      {showModal && (
+        <Modal onClose={closeModal}>
+          <UploadPostForm />
+        </Modal>
+      )}
+    </>
+  )
+}
