0d89620a99f37dd116ad4f55e309ff9750788556
Chris Young <ChrisC.Young1@gmail.com>
2022-03-08 14:46:46 -0800

    still testing delte likes

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, session, request
 from flask_login import current_user
-from app.models import User, Post, Comment, db
+from app.models import User, Post, Comment, db, likes
 from app.forms.post_form import PostForm
 
 
@@ -24,8 +24,8 @@ def create_like():
 @like_routes.route('/', methods=['DELETE'])
 def delete_like():
     print('------------------')
-    # user_id = current_user.get_id()
-    user_id = 2
+    user_id = current_user.get_id()
+    # user_id = 2
     data = request.json
     post_id = data['post_id']
 
@@ -35,9 +35,18 @@ def delete_like():
     # print('post.likers[0].id------------------', post.likers[0].id)
     # print('-----------------------------',int(post.likers[0].id) == int(user_id))
     liker_to_delete = [user for user in post.likers if int(user.id) == int(user_id)][0]
+    i = None
+    for index, item in enumerate(post.likers):
+        if int(item.id) == int(user_id):
+            i = index
+    # print('*******************',i)
     print('-------------------',liker_to_delete)
-    post.likers.remove(liker_to_delete)
-
+    print('----------------', post.likers)
+    # like = likes.query.filter(likes.user_id == user_id)
+    print('^^^^^^^^^^^^^^^^^^^^^^',likes)
+    post.likers.remove(post.likers[i])
+    print('--------------', post.likers)
+    # db.session.add(post)
     db.session.commit()
     # return {'post': post.to_dict()}, 200
     return { 'message': 'success'}
