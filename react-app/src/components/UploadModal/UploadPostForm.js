5cd4a7c1d6c7282e72f49366ac37bf2aaeb14808
jstnswn <jstnswn@gmail.com>
2022-03-08 12:49:07 -0800

    Add drag and drop feature

--- react-app/src/components/UploadModal/UploadPostForm.js
+++ react-app/src/components/UploadModal/UploadPostForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { FileUploader } from 'react-drag-drop-files';
 import { useDispatch } from 'react-redux'
 import { createPost } from '../../store/dashboard';
 import './UploadPostForm.css'
@@ -8,11 +9,6 @@ export default function UploadPostForm() {
   const [image, setImage] = useState(null);
   const [caption, setCaption] = useState('');
   const [errors, setErrors] = useState([]);
-
-  const handleFileInput = (e) => {
-    const file = e.target.files[0];
-    setImage(file);
-  };
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -24,34 +20,64 @@ export default function UploadPostForm() {
 
     // TODO error handling
     const data = await dispatch(createPost(payload))
-      if (data) {
-        setErrors([data]);
-      }
+    if (data) {
+      setErrors([data]);
+    }
+  };
+
+  const fileTypes  = ['PDF', 'PNG', 'JPG', 'JPEG'];
+
+  const hiddenInputRef = useRef(null);
+
+  const handleClick = (e) => {
+    hiddenInputRef.current.click();
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    setImage(file);
   };
 
   return (
-    <form
-      className='upload-post-form form'
-      onSubmit={handleSubmit}
-    >
-      <h2>Select an image to upload</h2>
-      <ul>
-        {errors && (
-          errors.map((error, idx) => <li key={idx}>{error}</li>)
-      )}
-      </ul>
-      <label>Image</label>
-      <input
-        type='file'
-        onChange={handleFileInput}
-      />
-      <label>Caption (optional)</label>
-      <textarea
-        type='text'
-        value={caption}
-        onChange={e => setCaption(e.target.value)}
-      />
-      <button>Post</button>
-    </form>
+    <div id='post-form-container'>
+      <h3>Create new post</h3>
+      <form
+        className='upload-post-form form'
+        onSubmit={handleSubmit}
+      >
+        <ul>
+          {errors && (
+            errors.map((error, idx) => <li key={idx}>{error}</li>)
+          )}
+        </ul>
+        <FileUploader
+          handleChange={(file) => setImage(file) }
+          name='image'
+          types={fileTypes}
+        />
+        <button
+          onClick={handleClick}
+          className='upload-image-button'
+        >
+          Select from computer
+        </button>
+        <input
+          type='file'
+          ref={hiddenInputRef}
+          onChange={handleFileChange}
+          style={{display: 'none'}}
+        />
+
+      </form>
+    </div>
   )
 }
+
+        // <label>Caption (optional)</label>
+        // <textarea
+        //   type='text'
+        //   value={caption}
+        //   onChange={e => setCaption(e.target.value)}
+        // />
+        // <p>{image ? `File name: ${image.name}` : ''}</p>
+        // <button>Post</button>
