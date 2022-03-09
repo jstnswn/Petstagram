e589deeb9cd9a5f44ce669950223703e3a8b04d3
jstnswn <jstnswn@gmail.com>
2022-03-08 17:08:46 -0800

    Post upload styling for caption seciton

--- react-app/src/components/UploadModal/UploadPostForm.js
+++ react-app/src/components/UploadModal/UploadPostForm.js
@@ -1,16 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { FileUploader } from 'react-drag-drop-files';
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { createPost } from '../../store/dashboard';
 import './UploadPostForm.css'
 
-export default function UploadPostForm() {
+export default function UploadPostForm({ closeModal }) {
   const dispatch = useDispatch();
   const [image, setImage] = useState(null);
   const [imageUrl, setImageUrl] = useState(null);
   const [showTextForm, setShowTextForm] = useState(false);
   const [caption, setCaption] = useState('');
   const [errors, setErrors] = useState([]);
+
+  const user = useSelector(({ session }) => session.user);
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -22,12 +24,13 @@ export default function UploadPostForm() {
 
     // TODO error handling
     const data = await dispatch(createPost(payload))
+      .then(() => closeModal())
     if (data) {
       setErrors([data]);
     }
   };
 
-  const fileTypes  = ['PDF', 'PNG', 'JPG', 'JPEG'];
+  const fileTypes = ['PDF', 'PNG', 'JPG', 'JPEG'];
 
   const hiddenInputRef = useRef(null);
 
@@ -61,15 +64,24 @@ export default function UploadPostForm() {
   let headerContent;
   if (!imageUrl) {
     headerContent = (
-      <h3>Create new post</h3>
+      <>
+        <h3>Create new post</h3>
+      </>
     )
-  }
-  if (imageUrl) {
+  } else if (imageUrl && !showTextForm) {
     headerContent = (
       <>
         <i className='fal fa-arrow-left' onClick={() => setImageUrl(null)}></i>
         <h3>Placeholder</h3>
         <p className='next-button' onClick={() => setShowTextForm(true)}>Next</p>
+      </>
+    )
+  } else {
+    headerContent = (
+      <>
+        <i className='fal fa-arrow-left' onClick={() => setImageUrl(null)}></i>
+        <h3>Create new post</h3>
+        <p className='next-button' onClick={handleSubmit}>Submit</p>
       </>
     )
   }
@@ -79,7 +91,7 @@ export default function UploadPostForm() {
     formContent = (
       <form
         className='upload-post-form form'
-        onSubmit={handleSubmit}
+      // onSubmit={handleSubmit}
       >
 
         <FileUploader
@@ -101,20 +113,50 @@ export default function UploadPostForm() {
         />
       </form>
     )
-  }
-
-  if (imageUrl) {
+  } else if (imageUrl && !showTextForm) {
     formContent = (
       <img
         alt='post content'
-        className='upload-image'
+        className='upload-image view'
         src={imageUrl}
       />
     );
+  } else {
+    formContent = (
+      <>
+        <div className='upload-form-container final'>
+          <div className='image-container'>
+            <img
+              alt='post content'
+              className='upload-image form'
+              src={imageUrl}
+            />
+          </div>
+
+          <div className='upload-form side-container'>
+            <div className='post-form user-info'>
+              <img
+                alt='profile avatar'
+                className='post-form profile-picture'
+                src={user.image_url}
+              />
+              <p className='post-form username'>{user.username}</p>
+            </div>
+            <textarea
+              className='comment-input'
+              placeholder='Write a caption'
+              value={caption}
+              onChange={e => setCaption(e.target.value)}
+            />
+          <p className='post-form word-count'>{`${caption.length}/2,200`}</p>
+          </div>
+        </div>
+      </>
+    )
   }
 
   return (
-    <div id='post-form-container'>
+    <div id='post-form-container' className={showTextForm ? 'final-form' : ''}>
       <div className='upload-post header'>
         {headerContent}
       </div>
@@ -123,16 +165,3 @@ export default function UploadPostForm() {
   )
 }
 
-{/* <ul>
-  {errors && (
-    errors.map((error, idx) => <li key={idx}>{error}</li>)
-  )}
-</ul> */}
-        // <label>Caption (optional)</label>
-        // <textarea
-        //   type='text'
-        //   value={caption}
-        //   onChange={e => setCaption(e.target.value)}
-        // />
-        // <p>{image ? `File name: ${image.name}` : ''}</p>
-        // <button>Post</button>
