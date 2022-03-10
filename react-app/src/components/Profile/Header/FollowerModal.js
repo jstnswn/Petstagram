dcc94480f173cf3e8c423bd9167cb0d8ce21f5d0
Chris Young <ChrisC.Young1@gmail.com>
2022-03-10 12:53:35 -0800

    saving changes

--- react-app/src/components/Profile/Header/FollowerModal.js
+++ react-app/src/components/Profile/Header/FollowerModal.js
@@ -2,10 +2,16 @@ import React from 'react'
 import './FollowerModal.css'
 import x_btn from '../../../assets/x.png'
 import { Link } from 'react-router-dom'
-
+import { useDispatch } from 'react-redux'
+import { follow } from '../../../store/dashboard'
 
 
 export default function FollowerFormModal({ user, closeModal }) {
+    const dispatch = useDispatch()
+    const userFollowing = user.following.map(user => user.id)
+
+    console.log(userFollowing)
+
     return (
         <>
             <div className='follower-heading'>
@@ -17,13 +23,20 @@ export default function FollowerFormModal({ user, closeModal }) {
             <div className='follower-modal-container'>
 
                 {user.followers.map(follower => (
-                    <div className='follower-info'>
-                        <img className='modal-img' src={follower.image_url}></img>
-                        <div className='follower-user-full'>
-                        <li className='modal-username'><Link onClick={closeModal} className='link' to={`/${follower.username}`}>{follower.username}</Link></li>
-                            <li className='modal-fullname'>{follower.full_name}</li>
+                        <div className='follower-info'>
+                            <img className='modal-img' src={follower.image_url}></img>
+                            <div className='follower-user-full'>
+                                <div className='username-follow-container'>
+                                    <Link onClick={closeModal} className='link' to={`/${follower.username}`}>
+                                        {follower.username}
+                                    </Link>
+                                    { !userFollowing.includes(follower.id) &&
+                                    <div className='modal-follow' onClick={() => dispatch(follow(follower.id))}>Follow</div>
+                                    }
+                                </div>
+                                <li className='modal-fullname'>{follower.full_name}</li>
+                            </div>
                         </div>
-                    </div>
                 ))}
             </div>
         </>
