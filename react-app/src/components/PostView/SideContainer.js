7db7470e3a238157b57a6b820706b38bf0dfc8d5
Srdan <srdan.vorkapic@outlook.com>
2022-03-10 22:53:01 -0800

    close post view

--- react-app/src/components/PostView/SideContainer.js
+++ react-app/src/components/PostView/SideContainer.js
@@ -75,7 +75,7 @@ function SideContainer({ post, closeMenu, closePostView, option}) {
                             <img className="post-view-user-img"src={post.user.image_url}></img>
                         </div>
                         <div>
-                            <NavLink to={`/${post.user.username}`}>{post.user.username}</NavLink>
+                            <NavLink to={`/${post.user.username}`} onClick={closePostView}>{post.user.username}</NavLink>
                             <span>{post.caption}</span>
                         </div>
                     </div>
@@ -88,7 +88,7 @@ function SideContainer({ post, closeMenu, closePostView, option}) {
                         <img className="post-view-user-img"src={comment.user.image_url}></img>
                     {/* </div>
                     <div> */}
-                        <NavLink to={`/${comment.user.username}`}>{comment.user.username}</NavLink>
+                        <NavLink to={`/${comment.user.username}`} onClick={closePostView}>{comment.user.username}</NavLink>
                         <span>{comment.comment}</span>
                         {comment.user.id === sessionUser.id ? <DeleteComment post={post} commentId={comment.id}/>     : null}
                     </div>
