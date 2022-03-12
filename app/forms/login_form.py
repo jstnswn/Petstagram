3d8c358fc385dd1ce9be2dc805df81663643e134
Srdan <srdan.vorkapic@outlook.com>
2022-03-11 22:53:14 -0800

    Login handling works

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -5,43 +5,64 @@ from app.models import User
 from sqlalchemy import or_
 
 
-def user_exists_email(form, field):
-    # Checking if user exists
-    email = field.data
-    if email == '':
-        return
+# def user_exists_email(form, field):
+#     # Checking if user exists
+#     email = field.data
+#     if email == '':
+#         return
 
-    user = User.query.filter(User.email == email).first()
+#     user = User.query.filter(User.email == email).first()
 
-    if not user:
-        raise ValidationError('Email provided not found.')
+#     if not user:
+#         raise ValidationError('Email provided not found.')
 
-def user_exists_username(form, field):
-    username = field.data
-    if username == '':
-        return
+# def user_exists_username(form, field):
+#     username = field.data
+#     if username == '':
+#         return
 
-    user = User.query.filter(User.username == username).first()
+#     user = User.query.filter(User.username == username).first()
+
+#     if not user or '':
+#         raise ValidationError('Username provided not found.')
+
+
+def check_credentials(form, field):
+    credentials = field.data
+    print(credentials)
+    print(form.data)
+    user = User.query.filter(or_(User.email == credentials, User.username == credentials)).first()
 
     if not user or '':
-        raise ValidationError('Username provided not found.')
+        raise ValidationError('User provided not found.')
 
 
-def password_matches(form, field):
-    # Checking if password matches
+# def password_matches(form, field):
+#     # Checking if password matches
+#     password = field.data
+#     email = form.data['email']
+#     username = form.data['username']
+#     user = User.query.filter(or_(User.email == email, User.username == username)).first()
+#     # print(user)
+#     if not user:
+#         raise ValidationError('No such user exists.')
+#     if not user.check_password(password):
+#         raise ValidationError('Password was incorrect.')
+
+
+def check_password(form, field):
     password = field.data
-    email = form.data['email']
-    username = form.data['username']
-    user = User.query.filter(or_(User.email == email, User.username == username)).first()
-    # print(user)
-    if not user:
-        raise ValidationError('No such user exists.')
-    if not user.check_password(password):
+    credentials = form.data['credentials']
+    user = User.query.filter(or_(User.email == credentials, User.username == credentials)).first()
+    if not user or '':
+        return
+    elif not user.check_password(password):
         raise ValidationError('Password was incorrect.')
 
 
+
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[user_exists_email])
-    username = StringField('username', validators=[user_exists_username])
-    password = StringField('password', validators=[
-                           DataRequired(), password_matches])
+    # email = StringField('email', validators=[user_exists_email])
+    # username = StringField('username', validators=[user_exists_username])
+    credentials = StringField('credentials', validators=[DataRequired(), check_credentials])
+    password = StringField('password', validators=[DataRequired(), check_password])
