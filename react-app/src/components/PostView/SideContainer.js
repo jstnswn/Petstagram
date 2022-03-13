210f84501538fe2fb9b34aed262e13982f90b955
David Lee <davidlee777@gmail.com>
2022-03-13 05:40:26 -0500

    minor changes

--- react-app/src/components/PostView/SideContainer.js
+++ react-app/src/components/PostView/SideContainer.js
@@ -41,7 +41,7 @@ function SideContainer({ post, closeMenu, closePostView, option, profileUser }) 
 
         if (pastSecond <= 60 && pastMinute === 0 && pastHour === 0 && pastDay === 0) return `< 1m`;
         if (pastMinute <= 60 && pastHour === 0 && pastDay === 0) return `${pastMinute}m`;
-        if (pastHour <= 60 && pastDay === 0) return `${pastHour}h`;
+        if (pastHour <= 60 && pastDay === 0) return `${pastHour-5}h`;
         if (pastDay >= 2 || pastHour > 24) return `${pastDay}d`;
 
     }
